82ba8e6ed5a880735e8a63b5a05867ce741f9fa0
sarvagra <sarvagras977@gmail.com>
2024-07-14 15:57:54 +0530

    updated files

---
+++ download.py
@@ -0,0 +1,4 @@
+import urllib.request
+url = "https://audio.iskcondesiretree.com/index.php?q=f&f=%2F02_-_ISKCON_Swamis%2FISKCON_Swamis_-_R_to_Y%2FHis_Holiness_Radha_Govinda_Swami%2F00_-_Canto-Wise_Katha"
+save_path = "C:\Users\sarva\Downloads\canto"
+urllib.request.urlretrieve(url, save_path)
